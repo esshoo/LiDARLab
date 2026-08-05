dc111608afcfc6c61946de1e14697488b101c518
esshoo <esshoo@gmail.com>
2026-08-05 21:56:42 +0300

    Downgrad

--- LiDARLab/Core/Models/LiDARFeature.swift
+++ LiDARLab/Core/Models/LiDARFeature.swift
@@ -46,7 +46,7 @@ enum LiDARFeature: String, CaseIterable, Identifiable, Hashable {
         case .planeDetection: "اكتشاف المستويات"
         case .arPlayground: "مختبر الواقع المعزز"
         case .depthPhoto: "صورة مع العمق"
-        case .computerBridge: "الموقع والمسح الثابت"
+        case .computerBridge: "البث إلى الكمبيوتر"
         case .roomScan: "مسح الغرف"
         case .sensorTests: "اختبارات الحساس"
         case .recordings: "تسجيل الجلسات"
@@ -67,7 +67,7 @@ enum LiDARFeature: String, CaseIterable, Identifiable, Hashable {
         case .planeDetection: "اكتشاف الأسطح الأفقية والرأسية"
         case .arPlayground: "وضع وتحريك مجسمات داخل المكان"
         case .depthPhoto: "حفظ الصورة وخريطة العمق معًا"
-        case .computerBridge: "تحديد الموقع أولًا، مسح 2D ثانيًا، ومعالجة محلية بعد الإنهاء"
+        case .computerBridge: "إرسال موقع الهاتف مباشرة إلى برنامج Windows"
         case .roomScan: "مسح كل غرفة منفصلة وتثبيتها قبل الانتقال"
         case .sensorTests: "اختبار ثبات قراءة العمق وتذبذبها"
         case .recordings: "حفظ إطارات الصورة والعمق وإعادة عرضها"
@@ -88,7 +88,7 @@ enum LiDARFeature: String, CaseIterable, Identifiable, Hashable {
         case .planeDetection: "viewfinder.rectangular"
         case .arPlayground: "arkit"
         case .depthPhoto: "camera.filters"
-        case .computerBridge: "location.viewfinder"
+        case .computerBridge: "desktopcomputer.and.arrow.down"
         case .roomScan: "house.lodge"
         case .sensorTests: "waveform.path.ecg"
         case .recordings: "record.circle"
@@ -110,9 +110,9 @@ enum LiDARFeature: String, CaseIterable, Identifiable, Hashable {
 
     var requirement: FeatureRequirement {
         switch self {
-        case .deviceInfo, .exportCenter, .computerBridge:
+        case .deviceInfo, .exportCenter:
             .none
-        case .angleMeasure, .levelTool, .planeDetection, .arPlayground:
+        case .angleMeasure, .levelTool, .planeDetection, .arPlayground, .computerBridge:
             .worldTracking
         case .depthCamera, .distanceMeasure, .pointCloud, .depthPhoto,
              .sensorTests, .recordings:
@@ -154,7 +154,7 @@ enum LiDARFeature: String, CaseIterable, Identifiable, Hashable {
         case .depthPhoto:
             ["الصورة الأصلية", "خريطة العمق", "تأثيرات الضباب والعزل"]
         case .computerBridge:
-            ["تسجيل كل Pose قبل أي عمل آخر", "Depth بنفس Frame ID", "معالجة محلية بعد فصل الالتقاط"]
+            ["إرسال Pose الحقيقي", "اختيار IP ومعدل الإرسال", "مراقبة التتبع والحرارة"]
         case .roomScan:
             ["جلسة مستقلة لكل غرفة", "ARSession مشتركة بين الغرف", "حفظ الغرف المجمدة ونموذج دمج للمقارنة"]
         case .recordings:
--- LiDARLab/Features/Home/FeatureRouterView.swift
+++ LiDARLab/Features/Home/FeatureRouterView.swift
@@ -33,7 +33,7 @@ struct FeatureRouterView: View {
             case .depthPhoto:
                 DepthPhotoView()
             case .computerBridge:
-                StableScanView()
+                ComputerBridgeView()
             case .roomScan:
                 RoomScanView()
             case .sensorTests:
